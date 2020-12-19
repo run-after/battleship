e272c1b7f429fbc5081668b831e188d68854fb6c
run-after <run.after@protonmail.com>
2020-12-19 12:05:55 -0600

    Correct logic to not allow same move twice

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -2,21 +2,23 @@ const playerFactory = () => {
   const moves = [];
 
   const computerPlay = () => {
-    const xCoord = Math.floor(Math.random() * 9); 
+    const xCoord = Math.floor(Math.random() * 9);
     const yCoord = Math.floor(Math.random() * 10) + 1;
+    let move = [xCoord, yCoord];
     // don't allow same move twice;
-    if (moves.includes(`${xCoord},${yCoord}`)) { 
-      computerPlay();
+    if (moves.includes(`${xCoord},${yCoord}`)) {
+      move = computerPlay();
+      console.log('repeat')
     };
-    moves.push(`${xCoord},${yCoord}`)
-    return [xCoord, yCoord]
-  }
+    moves.push(`${move[0]},${move[1]}`);
+    return move;
+  };
 
   return {
     playedMoves: moves,
     computerPlay: computerPlay
-  }
+  };
 
-}
+};
 
 export default playerFactory;
