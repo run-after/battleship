50d59c8798a7986de67359d4c1d5b98f34265271
run-after <run.after@protonmail.com>
2020-12-21 14:22:56 -0600

    Add disable click after already clicked, add computer play after click, and clean up some implementation

--- src/gameboardDOM.js
+++ src/gameboardDOM.js
@@ -1,4 +1,4 @@
-const gameboardDOM = (compBoard, playerBoard, round) => {
+const gameboardDOM = (compBoard, playerBoard, playRound, isGameOver) => {
   
   const boardSetup = (board, boardDOM) => {
     // loop through whole array
@@ -12,7 +12,11 @@ const gameboardDOM = (compBoard, playerBoard, round) => {
       board[key].map((x, i) => {
         let space = document.createElement('div');
         space.classList.add('space');
-        space.setAttribute('data-coord', `${i},${key}`)
+        space.setAttribute('data-coord', `${i},${key}`);
+        // Show players ships
+        if (boardDOM === playerBoardDOM && x) {
+          space.style.background = 'yellow'
+        };
         
         row.appendChild(space);
       });
@@ -21,41 +25,51 @@ const gameboardDOM = (compBoard, playerBoard, round) => {
 
   const boardKeys = Object.keys(playerBoard.board);
 
+  const playerBoardDOM = document.querySelector('.player-board');
+  playerBoardDOM.textContent = 'Player Board';
+  boardSetup(playerBoard.board, playerBoardDOM);
+
   const computerBoardDOM = document.querySelector('.computer-board');
   computerBoardDOM.textContent = 'Computer Board';
   boardSetup(compBoard.board, computerBoardDOM);
 
-  const playerBoardDOM = document.querySelector('.player-board');
-  playerBoardDOM.textContent = 'Player Board';
-  boardSetup(playerBoard.board, playerBoardDOM);
-  
-  const addListener = (board, e) => {
+  const handleClick = (board, e) => {
+    
     // Only add listener to spaces on board
     if (e.target.classList[0] === 'space') {
       const coords = e.target.dataset.coord.split(',');
-    
+      
       let x = Number(coords[0]);
       let y = Number(coords[1]);
 
       const attack = board.receiveAttack(x, y);
-      console.log(board.board[y][x][0].isSunk())
+      
+      e.target.classList.add('disable-clicks');
+
       if (attack === 'hit') {
-        e.target.style = 'background: red';
+        const hit = document.createElement('div');
+        hit.classList.add('hit');
+        e.target.appendChild(hit);
       } else if (attack === 'miss') {
         e.target.style = 'background: blue';
       };
-      
-      // don't increment round if 'already guessed'
-      round++;
-    };
+
+      return attack;
+    }; 
   };
 
   computerBoardDOM.addEventListener('click', (e) => {
-    addListener(compBoard, e);
+    const result = handleClick(compBoard, e);
+    const compMove = playRound().split(',');
+    let square = playerBoardDOM.querySelector(`[data-coord='${compMove[0]},${compMove[1]}']`);
+
+    if(result && !isGameOver()) {
+      setTimeout(()=>square.click(), 500);  
+    };
   });
   
   playerBoardDOM.addEventListener('click', (e) => {
-    addListener(playerBoard, e);
+    handleClick(playerBoard, e);
   });
 
 };
